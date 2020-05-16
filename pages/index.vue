5f7f7a70002eb27727a113245202d015c3636d4f
Da-max <da-max@tutanota.com>
2020-05-16 11:26:50 +0200

    Add head in index page.

--- pages/index.vue
+++ pages/index.vue
@@ -124,6 +124,24 @@ export default {
   name: 'Index',
   components: {
     CustomFontAwesomeIcon
+  },
+  head() {
+    return {
+      title: 'Accueil | Fedabian',
+      meta: [
+        {
+          hid: 'description-page',
+          name: 'description',
+          content: 'Portfolio de Maxime `Da-max` Ben Hassen, développeur web.'
+        },
+        {
+          hid: 'keywords',
+          name: 'keywords',
+          content:
+            'Maxime `Da-max` Ben Hassen, développeur web, maxime benhassen, portfolio da-max, Maxime Ben Hassen portfolio'
+        }
+      ]
+    }
   }
 }
 </script>
