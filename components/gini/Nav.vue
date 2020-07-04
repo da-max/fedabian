0996c8d085c1c19f5da0307437f67e25ae259254
Da-max <da-max@tutanota.com>
2020-07-04 23:58:20 +0200

    Start to write " gini " app.

---
+++ components/gini/Nav.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul class="uk-nav uk-nav-default">
+    <li>
+      <nuxt-link to="/donnees/gini/definition"
+        >Qu’est ce que le coefficient de Gini ?</nuxt-link
+      >
+      <nuxt-link to="/donnees/gini/monde"
+        >Le coefficient de Gini dans le monde</nuxt-link
+      >
+      <nuxt-link to="/donnees/gini/pays"
+        >Le coefficient de Gini par pays</nuxt-link
+      >
+    </li>
+    <li class="uk-nav-divider"></li>
+    <li><nuxt-link to="/donnees/gini/a-propos">A propos</nuxt-link></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'GiniNav'
+}
+</script>
---
+++ pages/donnees/gini/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="uk-margin-large-top">
+    <h1 class="uk-text-center uk-text-bold uk-margin-large-bottom">
+      Coefficient de Gini
+    </h1>
+
+    <section
+      class="uk-container uk-background-default uk-padding uk-margin-large-bottom"
+    >
+      <gini-nav></gini-nav>
+    </section>
+  </main>
+</template>
+
+<script>
+import GiniNav from '~/components/gini/Nav'
+export default {
+  components: {
+    GiniNav
+  }
+}
+</script>
