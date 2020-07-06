f58256d57204c70cb88217d3d5e8407b858ebf0e
Da-max <da-max@tutanota.com>
2020-07-06 13:07:17 +0200

    Update Nav of gini app.

--- components/gini/Nav.vue
+++ components/gini/Nav.vue
@@ -1,19 +1,21 @@
 <template>
-  <ul class="uk-nav uk-nav-default">
-    <li>
-      <nuxt-link to="/donnees/gini/definition"
-        >Qu’est ce que le coefficient de Gini ?</nuxt-link
-      >
-      <nuxt-link to="/donnees/gini/monde"
-        >Le coefficient de Gini dans le monde</nuxt-link
-      >
-      <nuxt-link to="/donnees/gini/pays"
-        >Le coefficient de Gini par pays</nuxt-link
-      >
-    </li>
-    <li class="uk-nav-divider"></li>
-    <li><nuxt-link to="/donnees/gini/a-propos">A propos</nuxt-link></li>
-  </ul>
+  <nav>
+    <ul class="uk-nav uk-nav-default">
+      <li>
+        <nuxt-link to="/donnees/gini/definition"
+          >Qu’est ce que le coefficient de Gini ?</nuxt-link
+        >
+        <nuxt-link to="/donnees/gini/monde"
+          >Le coefficient de Gini dans le monde</nuxt-link
+        >
+        <nuxt-link to="/donnees/gini/pays"
+          >Le coefficient de Gini par pays</nuxt-link
+        >
+      </li>
+      <li class="uk-nav-divider"></li>
+      <li><nuxt-link to="/donnees/gini/a-propos">A propos</nuxt-link></li>
+    </ul>
+  </nav>
 </template>
 
 <script>
