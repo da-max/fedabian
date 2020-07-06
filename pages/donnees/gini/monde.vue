c6be7d351f3432c103da56b7afffbe66170674b8
Da-max <da-max@tutanota.com>
2020-07-06 21:19:24 +0200

    Update monde page.

--- pages/donnees/gini/monde.vue
+++ pages/donnees/gini/monde.vue
@@ -5,16 +5,16 @@
     </h2>
     <hr />
     <ul uk-accordion="multiple: true">
-      <li class="uk-open">
-        <a class="uk-accordion-title">Carte du coefficient de Gini</a>
-        <div class="uk-accordion-content">
-          <gini-map></gini-map>
-        </div>
-      </li>
       <li>
         <a class="uk-accordion-title">Le coefficient de Gini par pays</a>
         <div class="uk-accordion-content uk-overflow-auto">
           <gini-table></gini-table>
+        </div>
+      </li>
+      <li class="uk-open">
+        <a class="uk-accordion-title">Carte du coefficient de Gini</a>
+        <div class="uk-accordion-content">
+          <gini-map></gini-map>
         </div>
       </li>
     </ul>
