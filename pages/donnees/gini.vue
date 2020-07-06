fcdc7d96b012ef337bb6e24b73c0c18e6d297cdb
Da-max <da-max@tutanota.com>
2020-07-06 13:08:31 +0200

    Add gini page.

---
+++ pages/donnees/gini.vue
@@ -0,0 +1,15 @@
+<template>
+  <nuxt-child></nuxt-child>
+</template>
+
+<script>
+export default {
+  name: 'Gini',
+  async fetch() {
+    const { data } = await this.$axios.get(
+      'https://restcountries.eu/rest/v2/all'
+    )
+    this.$store.commit('gini/SET_COUNTRIES', data)
+  }
+}
+</script>
