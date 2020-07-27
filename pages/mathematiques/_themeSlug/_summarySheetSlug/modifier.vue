16ab4a87da735c80f335f176c1ffc437af25ad0e
Da-max <da-max@tutanota.com>
2020-07-27 18:27:24 +0200

    Move mathematics admin pages to mathematics directory (it’s more simple for use store).

---
+++ pages/mathematiques/_themeSlug/_summarySheetSlug/modifier.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <summary-sheet-form
+      :summary-sheet-id="summarySheet.id"
+    ></summary-sheet-form>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import SummarySheetForm from '~/components/mathematics/SummarySheetForm'
+
+export default {
+  name: 'UpdateSummarySheet',
+  middleware: ['auth'],
+  components: { SummarySheetForm },
+  computed: {
+    ...mapGetters({ summarySheetBySlug: 'mathematics/summarySheetBySlug' }),
+    summarySheet() {
+      return this.summarySheetBySlug(this.$route.params.summarySheetSlug)
+    }
+  }
+}
+</script>
---
+++ pages/mathematiques/ajouter-une-fiche.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <header class="uk-margin-large-bottom uk-text-center">
+      <h1>Ajouter une fiche de révision</h1>
+    </header>
+    <summary-sheet-form></summary-sheet-form>
+  </div>
+</template>
+
+<script>
+import SummarySheetForm from '~/components/mathematics/SummarySheetForm'
+
+export default {
+  name: 'AddSummarySheet',
+  middleware: ['auth'],
+  components: { SummarySheetForm }
+}
+</script>
