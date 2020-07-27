40ef5f2edd3663879cb51f138f3d88689e88ede8
Da-max <da-max@tutanota.com>
2020-07-27 12:44:40 +0200

    Add two utility components (input, modal).

---
+++ components/utility/Modal.vue
@@ -0,0 +1,81 @@
+<template>
+  <div
+    v-if="bgClose === true"
+    :class="{ 'uk-flex-top ': center, 'uk-modal-container': container }"
+    uk-modal
+    bg-close="true"
+    class="uk-light"
+  >
+    <div class="uk-modal-dialog">
+      <button
+        v-show="closeButton"
+        class="uk-modal-close-default"
+        :class="{ 'uk-magin-auto-vertical': center }"
+        type="button"
+        uk-close
+      ></button>
+      <div class="uk-modal-header">
+        <slot name="header"></slot>
+      </div>
+      <div class="uk-modal-body" uk-overflow-auto>
+        <slot name="body"></slot>
+      </div>
+      <div class="uk-modal-footer uk-text-center uk-padding">
+        <slot name="footer">
+          <button
+            class="uk-button uk-button-default uk-modal-close"
+            type="button"
+          >
+            Fermer
+          </button>
+        </slot>
+      </div>
+    </div>
+  </div>
+  <div
+    v-else
+    id="modal"
+    :class="{ 'uk-flex-top ': center, 'uk-modal-container': container }"
+    uk-modal
+    bg-close="false"
+  >
+    <div class="uk-modal-dialog">
+      <button
+        v-show="closeButton"
+        class="uk-modal-close-default"
+        :class="{ 'uk-magin-auto-vertical': center }"
+        type="button"
+        uk-close
+      ></button>
+      <div class="uk-modal-header">
+        <slot name="header"></slot>
+      </div>
+      <div class="uk-modal-body" uk-overflow-auto>
+        <slot name="body"></slot>
+      </div>
+      <div class="uk-modal-footer uk-text-center uk-padding">
+        <slot name="footer">
+          <button
+            class="uk-button uk-button-default uk-modal-close"
+            type="button"
+          >
+            Fermer
+          </button>
+        </slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+
+  props: {
+    closeButton: { type: Boolean, default: true },
+    center: { type: Boolean, default: false },
+    container: { type: Boolean, default: false },
+    bgClose: { type: Boolean, default: true }
+  }
+}
+</script>
---
+++ components/utility/form/Input.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <label :for="name" class="uk-form-label">{{ label }}</label>
+    <div class="uk-form-controls">
+      <input
+        :type="type"
+        :name="name"
+        class="uk-input"
+        :placeholder="label"
+        :value="value"
+        @input="$emit('input', $event.target.value)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputForm',
+  props: {
+    name: {
+      type: String,
+      default: 'name',
+      required: true
+    },
+    type: {
+      type: String,
+      default: 'text'
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
