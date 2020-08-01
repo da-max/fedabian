15a029434fcd640a4a8b4c515431f1a583e67205
Da-max <da-max@tutanota.com>
2020-08-01 17:46:28 +0200

    Update mathematicsControlers for don’t use asyncLib.

--- server/routes/mathematicsControlers.js
+++ server/routes/mathematicsControlers.js
@@ -1,199 +1,131 @@
-const asyncLib = require('async')
 const models = require('../models')
 
 module.exports = {
   async summarySheetsList(_req, res) {
-    const summarySheets = await models.SummarySheet.findAll({
-      attributes: ['id', 'name', 'path', 'themeId', 'slug']
-    }).catch(() => {
+    try {
+      const summarySheets = await models.SummarySheet.findAll({
+        attributes: ['id', 'name', 'path', 'themeId', 'slug']
+      })
+      return res.status(200).json(summarySheets)
+    } catch {
       return res.status(500).json({ error: 'cannot get summary sheets.' })
-    })
-
-    return res.status(200).json(summarySheets)
+    }
   },
 
   async themesList(_req, res) {
-    const themes = await models.Theme.findAll({
-      attributes: ['id', 'name', 'slug']
-    }).catch(() =>
-      res.status(500).json({ error: 'cannot get mathematics themes.' })
-    )
-
-    return res.status(200).json(themes)
+    try {
+      const themes = await models.Theme.findAll({
+        attributes: ['id', 'name', 'slug']
+      })
+      return res.status(200).json(themes)
+    } catch {
+      return res.status(500).json({ error: 'cannot get mathematics themes.' })
+    }
   },
 
-  summarySheetRetrive(req, res) {
+  async summarySheetRetrive(req, res) {
     const summarySheetSlug = req.params.summarySheetSlug
-      ? req.params.summarySheetSlug
-      : null
 
-    asyncLib.waterfall([
-      (done) => {
-        models.SummarySheet.findOne({
-          attributes: ['id', 'name', 'path'],
-          where: { slug: summarySheetSlug }
-        })
-          .then((summarySheet) => {
-            done(null, summarySheet)
-          })
-          .catch(() =>
-            res.status(500).json({ error: 'unable to get summary sheet.' })
-          )
-      },
-      (summarySheet) => {
-        if (summarySheet) {
-          return res.status(200).json(summarySheet)
-        } else {
-          return res.status(403).json({ error: 'summary sheet not found.' })
-        }
+    try {
+      const summarySheet = await models.SummarySheet.findOne({
+        attributes: ['id', 'name', 'path'],
+        where: { slug: summarySheetSlug }
+      })
+      if (summarySheet) {
+        return res.status(200).json(summarySheet)
+      } else {
+        return res.status(403).json({ error: 'summary sheet not found.' })
       }
-    ])
+    } catch {
+      return res.status(500).json({ error: 'unable to get summary sheet.' })
+    }
   },
 
-  themeCreate(req, res) {
+  async themeCreate(req, res) {
     const name = req.body.name
     const slug = req.body.slug
 
-    asyncLib.waterfall([
-      (done) => {
-        models.Theme.findOne({
-          attributes: ['id', 'name', 'slug'],
-          where: { slug }
+    try {
+      const themeFound = await models.Theme.findOne({
+        attributes: ['id', 'name', 'slug'],
+        where: { slug }
+      })
+      if (!themeFound) {
+        const newTheme = await models.Theme.create({
+          name,
+          slug
         })
-          .then((themeFound) => {
-            done(null, themeFound)
-          })
-          .catch(() =>
-            res.status(500).json({ error: 'unable to verify theme.' })
-          )
-      },
-      (themeFound, done) => {
-        if (!themeFound) {
-          models.Theme.create({
-            name,
-            slug
-          })
-            .then((newTheme) => {
-              done(null, newTheme)
-            })
-            .catch((err) =>
-              res.status(500).json({ error: `${err} cannot add theme.` })
-            )
-        } else {
-          return res.status(409).json({ error: 'theme already exist.' })
-        }
-      },
-      (newTheme) => {
-        if (newTheme) {
-          return res
-            .status(201)
-            .json({ id: newTheme.id, name: newTheme.name, slug: newTheme.slug })
-        } else {
-          return res.status(500).json({ error: 'cannot add theme.' })
-        }
+        return res
+          .status(201)
+          .json({ id: newTheme.id, name: newTheme.name, slug: newTheme.slug })
+      } else {
+        return res.status(409).json({ error: 'theme already exist.' })
       }
-    ])
+    } catch (err) {
+      res.status(500).json({ error: `${err} cannot add theme.` })
+    }
   },
 
-  summarySheetCreate(req, res) {
+  async summarySheetCreate(req, res) {
     const name = req.body.name
     const path = req.body.path
     const slug = req.body.slug
     const themeId = req.body.themeId
 
-    console.log(themeId)
+    try {
+      const summarySheetFound = await models.SummarySheet.findOne({
+        attributes: ['id', 'name', 'path', 'slug'],
+        where: { slug }
+      })
 
-    asyncLib.waterfall([
-      (done) => {
-        models.SummarySheet.findOne({
-          attributes: ['id', 'name', 'path', 'slug'],
-          where: { slug }
+      if (!summarySheetFound) {
+        const themeFound = await models.Theme.findOne({
+          attributes: ['name', 'slug'],
+          where: { id: themeId }
         })
-          .then((summarySheetFound) => {
-            done(null, summarySheetFound)
-          })
-          .catch(() =>
-            res.status(500).json({ error: 'unable to verify summary sheets.' })
-          )
-      },
-      (summarySheetFound, done) => {
-        if (!summarySheetFound) {
-          models.Theme.findOne({
-            attributes: ['name', 'slug'],
-            where: { id: themeId }
-          })
-            .then((themeFound) => {
-              console.log('OK')
-              done(null, themeFound)
-            })
-            .catch((error) => {
-              console.log(error)
-              return res.status(500).json({ error: 'unable to get theme' })
-            })
-        } else {
-          return res.status(409).json({ error: 'summary sheet already exist.' })
-        }
-      },
-      (themeFound, done) => {
-        console.log(themeFound)
         if (themeFound) {
-          models.SummarySheet.create({
+          const summarySheet = await models.SummarySheet.create({
             name,
             path,
             slug,
             themeId
           })
-            .then((summarySheet) => res.status(201).json(summarySheet))
-            .catch(() =>
-              res.status(500).json({ error: 'cannot add summary sheet.' })
-            )
+          return res.status(201).json(summarySheet)
         } else {
           return res.status(409).json({ error: 'cannot get theme.' })
         }
+      } else {
+        return res.status(409).json({ error: 'summary sheet already exist.' })
       }
-    ])
+    } catch {
+      return res.status(500).json({ error: 'unable to verify summary sheets.' })
+    }
   },
 
-  summarySheetUpdate(req, res) {
+  async summarySheetUpdate(req, res) {
     const name = req.body.name
     const slug = req.params.summarySheetSlug
     const path = req.body.path
     const themeId = req.body.themeId
 
-    console.log(req.body)
-
-    asyncLib.waterfall([
-      (done) => {
-        models.SummarySheet.findOne({
-          attributes: ['id', 'name', 'slug', 'path', 'themeId'],
-          where: { slug }
+    try {
+      const summarySheetFound = await models.SummarySheet.findOne({
+        attributes: ['id', 'name', 'slug', 'path', 'themeId'],
+        where: { slug }
+      })
+      if (summarySheetFound) {
+        await summarySheetFound.update({
+          name,
+          path,
+          slug,
+          themeId
         })
-          .then((summarySheetFound) => {
-            done(null, summarySheetFound)
-          })
-          .catch(() =>
-            res.status(500).json({ error: 'cannot find summary sheet.' })
-          )
-      },
-      (summarySheetFound) => {
-        if (summarySheetFound) {
-          summarySheetFound
-            .update({
-              name,
-              path,
-              slug,
-              themeId
-            })
-            .then(() =>
-              res.status(201).json({ message: 'summary sheet updated.' })
-            )
-            .catch(() =>
-              res.status(500).json({ error: 'cannot update project' })
-            )
-        } else {
-          return res.status(403).json({ error: 'summary sheet not found.' })
-        }
+        return res.status(201).json({ message: 'summary sheet update.' })
+      } else {
+        return res.status(403).json({ error: 'summary sheet not found.' })
       }
-    ])
+    } catch (error) {
+      return res.status(500).json({ error: 'cannot update summary sheet.' })
+    }
   }
 }
