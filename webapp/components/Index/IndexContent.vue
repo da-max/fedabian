f51be722ec0823f3e6f31d3ca7ea5b77403d34d9
da-max <da-max@tutanota.com>
2021-02-13 09:32:08 +0100

    chore(webapp): create index page

---
+++ webapp/components/Index/IndexContent.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class="skew">
+    <article class="md:w-2/3 m-auto">
+      <header>
+        <h2 class="text-3xl">
+          Créer des sites web afin de mettre en avant des projets éthiques et solidaires.
+        </h2>
+        <p>
+          Animé par des valeurs, les sites que je crée respecteront la vie privée de l’utilisateur.
+        </p>
+      </header>
+      <section>
+        <p class="mt-24 mb-12">
+          Je trouve que le numérique est le lieu de nombreuses dérives, je préfère donc limiter
+          les projets dans lesquels je m’engage afin qu’ils correspondent aux valeurs que j’essaie de porter.
+        </p>
+        <h2 class="my-5">
+          Liste (non exhaustive) des thèmes qui m’intéressent
+        </h2>
+        <ul class="list-disc">
+          <li>Open source, logiciel libre et décentralisation</li>
+          <li>Engagement politique et solidaire</li>
+          <li>Écologie</li>
+          <li>Circuit court et consommation éthique</li>
+          <li>Finance décentralisée</li>
+        </ul>
+      </section>
+      <footer class="mt-24 text-left">
+        <p>Ça vous intéresse ?</p>
+        <div class="text-center">
+          <nuxt-link to="/" class="btn btn-primary">
+            Me contacter
+          </nuxt-link>
+        </div>
+      </footer>
+    </article>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'DefaultNavbar'
+})
+
+</script>
+
+<style lang="scss" scoped>
+.skew {
+  @apply bg-blue-100 transform skew-y-3 p-20 my-20 text-center text-xl;
+  > * {
+    @apply transform -skew-y-3;
+  }
+}
+
+ul > * {
+  @apply mt-2;
+}
+</style>
---
+++ webapp/components/Index/IndexHeader.vue
@@ -0,0 +1,50 @@
+<template>
+  <header>
+    <div>
+      <h1 class="pacifico">
+        Maxime Ben Hassen
+      </h1>
+      <h2>Étudiant, passionné de logiciel libre, d’informatique et de mathématiques</h2>
+      <nuxt-link to="/" class="btn btn-primary">
+        Découvrir mes projets
+      </nuxt-link>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexHeader'
+})
+
+</script>
+
+<style lang="scss" scoped>
+header {
+  @apply h-full text-center;
+}
+
+h1, h2 {
+  @apply text-blue-800
+}
+
+h1 {
+  @apply text-5xl font-medium
+}
+
+h2 {
+  @apply text-4xl font-semibold mb-10
+}
+
+.btn {
+  @apply mt-8;
+}
+
+div {
+  @apply absolute transform -translate-x-1/2 -translate-y-1/2;
+  top: 50%;
+  left: 50%;
+}
+</style>
