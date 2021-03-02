890dc50de63c90cab825ccd8a3eb6264c30fb179
da-max <da-max@tutanota.com>
2021-03-02 17:44:34 +0100

    chore(webapp): adding types for graphql and vue files

---
+++ webapp/types/gql.d.ts
@@ -0,0 +1,13 @@
+import { DocumentNode } from 'graphql'
+
+declare module '*.gql' {
+
+  const content: DocumentNode
+  export default content
+}
+
+declare module '*.graphql' {
+
+  const content: DocumentNode
+  export default content
+}
